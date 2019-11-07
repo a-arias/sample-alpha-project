368a5fa7ea97ad76fdca65ea319170ded55997e6
Abraham Arias <arias.abram@gmail.com>
2019-11-06 22:06:09 -0700

    adding documentation to the tests

--- tests/e2e/specs/integration/commits/commits_spec.js
+++ tests/e2e/specs/integration/commits/commits_spec.js
@@ -4,40 +4,51 @@ describe('Commits Page', () => {
   });
 
   it('display commits list', () => {
+    // # Asserts contributors list is displayed on the page
     cy.get('[data-test-id="contributorsList"]').should('be.visible');
   });
 
   it('should be able to use pagination feature', () => {
+    // # Clicks next page button at the bottom
     cy.get('[data-test-id="buttonNextPage"]').click();
 
+    // # Assert previous page button is visible on the page
     cy.get('[data-test-id="buttonPreviousPage"]').should('be.visible');
   });
 
   it('should be able to show commits list filter by username', () => {
     let username = 'johnleider';
 
+    // # Clicks contributor list
     cy.get('[data-test-id="contributorsList"]').click({ force: true });
 
+    // # Clicks given username from the list
     cy.get('.v-list__tile__title').contains(username).click();
 
+    // # Asserts author username is displayed on the page
     cy.get('[data-test-id="commitAuthor"]').contains(username).should('be.visible');
   });
 
   it('should be able to show commits list filter by SHA', () => {
     let shaValue = '66e18555d5555835354b7c940929e1ef299319bc';
 
+    // # Writes on the search by SHA input text given SHA value
     cy.get('[data-test-id="shaInput"]').type(shaValue).type('{enter}');
 
+    // # Asserts first element of the list contains given SHA value and its displayed
     cy.contains(shaValue).first().should('be.visible');
   });
 
   it('should be able to navigate to commit page and verify commit information', () => {
     let shaValue = '4de4ef0a7e38f2b55dc804d3f253df49532dcdef';
 
+    // # Writes on the search by SHA input text given SHA value
     cy.get('[data-test-id="shaInput"]').type(shaValue);
 
+    // # Clicks first element of the list that contains given SHA value
     cy.contains(shaValue).first().click();
 
+    // # Asserts commit details information is present on the page
     cy.get('[data-test-id="changedFileText"]').should('be.visible').contains('packages/docs/src/lang/en/professional-support/Business.json')
   });
 })
--- tests/e2e/specs/integration/dashboard/dashboard_spec.js
+++ tests/e2e/specs/integration/dashboard/dashboard_spec.js
@@ -4,17 +4,22 @@ describe('Dashboard Page', () => {
   });
 
   it('should be able to display dashboard information', () => {
+    // # Asserts div with text Hello is present on the page
     cy.contains('div', 'Hello');
+    // # Asserts div with text Commits is present on the page
     cy.contains('div', 'Commits');
   });
 
   it('should be able to navigate to commits page', () => {
+    // # Clicks commits link on the left side menu
     cy.get('[data-test-id="commitsLinkPage"]').click();
 
+    // # Asserts contributors list is present on the page
     cy.get('[data-test-id="contributorsList"]').should('be.visible');
   });
 
   it('should be able to see login button', () => {
+    // # Asserts github login button is present on the page
     cy.get('[data-test-id="loginButton"]').should('be.visible');
   });
 })
